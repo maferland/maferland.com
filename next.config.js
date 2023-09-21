a0d07d7a1773bc2934d2206c9c241d9894c7180e
Marc-Antoine <me@maferland.com>
2023-09-21 11:25:40 -0400

    Fix deploy

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     domains: [
       // NextJS <Image> component needs to whitelist domains for src={}
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "images.unsplash.com",
-      "logos-world.net",
+      'lh3.googleusercontent.com',
+      'pbs.twimg.com',
+      'images.unsplash.com',
+      'logos-world.net',
     ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
